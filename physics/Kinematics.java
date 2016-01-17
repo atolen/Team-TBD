522e89d74d7785599f79241f4787d7be91ffe890
anna tolen <atolen@stuy.edu>
2016-01-17 18:41:58 -0500

    Kinematics added

---
+++ physics/Kinematics.java
@@ -0,0 +1,9 @@
+public class Kinematics extends Question {
+    Comparable d; // displacement
+    Comparable vi; // init velocity
+    Comparable vf; // final velocity
+    Comparable a; // acceleration
+    Comparable t; // time
+
+    
+} // close class Kinematics
