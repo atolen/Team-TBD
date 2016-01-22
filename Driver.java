efd67fbae93f51accfed1fabbc8195531c520a5d
gstempel <gstempel@stuy.edu>
2016-01-22 01:48:19 -0500

    driver added

---
+++ Driver.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+
+    //Instance Vars
+
+    //change these constants to choose which topics to focus on
+    public final static int kinQs = 2; // Kinematics
+    public final static int moQs = 2; // Motion
+    public final static int proQs = 2; // Projectiles
+    public final static int forQs = 2; // Force
+    public final static int momQs = 2; // Momentum
+    public final static int enQs = 2; // Energy
+    public final static int cirQs = 2; // Circular
+
+    private int qCount = 0;
+    private boolean gameOver;
+
+    private InputStreamReader isr;
+    private BufferedReader in;
+}
