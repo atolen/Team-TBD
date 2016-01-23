616d01b656fe6e038a78df970d538970690973dc
gstempel <gstempel@stuy.edu>
2016-01-23 12:18:15 -0500

    character work

--- story/Character.java
+++ story/Character.java
@@ -1,6 +1,15 @@
-public class Character {
+public abstract class Character {
 
     protected int hp;
-    protected int power;
+    protected String name;
+
+    //Constructor
+    public Character() {
+	hp = 100;
+	name = "Luke";
+    }
+
+    //Accessors
+
 
 }//end of class Character
